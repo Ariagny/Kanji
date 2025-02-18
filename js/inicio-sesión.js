50a8905fb9bec53d975759d7fe4f98e783f12114
Ariagny <ariagnyborregales01@gmail.com>
2025-02-18 15:49:58 -0500

    Inicio y registro de sesión

---
+++ js/inicio-sesión.js
@@ -0,0 +1,21 @@
+//inicio de sesion
+const Username = document.querySelector(".Username");
+const Password = document.querySelector(".Password");
+const formulario = document.querySelector(".formulario");
+
+//verificar usuario
+function validarUsuario (e){
+    e.preventDefault();
+    //llamar la información de local storage
+    let currentUser = JSON.parse(localStorage.getItem("user"));
+
+if (Username.value === currentUser.userN && Password.value === currentUser.userP){
+    window.location = "../index.html"
+}else {
+  alert("El usuario o la contraseña es incorrecta");
+}
+
+formulario.reset()
+}
+
+formulario.addEventListener("submit", validarUsuario)
---
+++ js/registro.js
@@ -0,0 +1,22 @@
+//registrar datos del usuario
+const Username = document.querySelector(".Username");
+const Password = document.querySelector(".Password");
+const formulario = document.querySelector(".formulario");
+const Correo = document.querySelector(".Correo");
+const Apellidos = document.querySelector(".Apellidos");
+
+function registrarUser(e){
+    e.preventDefault();
+    let user = {
+        userN: Username.value,
+        userP: Password.value,
+        userC: Correo.value,
+        userA: Apellidos.value,
+    }
+    //enviar información a local storage
+    localStorage.setItem("user", JSON.stringify(user));   
+    formulario.reset()
+    window.location = "../vistas/inicio-sesion.html"
+}
+
+formulario.addEventListener("submit",registrarUser)
