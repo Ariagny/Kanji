90d0393ee6215b75726db714c85bd1482cff8792
Ariagny <ariagnyborregales01@gmail.com>
2025-04-02 17:42:22 -0500

    Cambios

--- js/examen2.js
+++ js/examen2.js
@@ -13,6 +13,42 @@ for (let i = 0; i < usuarios.length; i++) {
   }
 } 
 
+const btnIniciarSesion = document.querySelector(".nav-link4");
+const btnRegistroSesion = document.querySelector(".nav-link5");
+const SaludoUsuario = document.querySelector(".nav-link6");
+const btnUsuario = document.querySelector(".nav-link7");
+const btnlist = document.querySelector(".nav-list");
+const BarrraNav = document.querySelector("#navbarNav");
+const btnCerrarSesion = document.querySelector(".nav-link8");
+
+
+//verifica si el usuario esta registrado
+function verificarSesion(){
+   /*  let currentUser = JSON.parse(localStorage.getItem('user')); */
+   for (let i = 0; i < usuarios.length; i++) {
+    let confirmarSesion = usuarios[i] ? usuarios[i].userLogged : false
+
+    if (confirmarSesion){
+        //si hay un usuario logueado, mostrarsu nombre y ocultar botones
+        SaludoUsuario.textContent =`Hola, ${usuarios[i].userN}`;
+        btnIniciarSesion.style.display = 'none';
+        btnRegistroSesion.style.display = 'none';
+        btnUsuario.style.display = 'flex';
+        btnCerrarSesion.style.display = "flex"  
+        return
+    }
+}
+SaludoUsuario.textContent = '';
+btnIniciarSesion.style.display = 'flex';
+btnRegistroSesion.style.display = 'flex';
+btnlist.classList.add('d-none');
+btnlist.classList.remove('d-flex');
+BarrraNav.style.justifyContent = "right"
+btnCerrarSesion.style.display = "none" 
+
+}
+
+document.addEventListener('DOMContentLoaded', verificarSesion);
 
 const PopUp = document.querySelector(".popup");
 const BtnPopUp = document.querySelector(".pop-btn");
